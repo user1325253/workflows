0a2cb55a6c63ab835f66695060b897e74d0eabaf
unknown <osullit@hotmail.com>
2020-05-08 14:00:40 -0600

    Added CoffeScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', async function () {
-    gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', async function () {
+    gulp.src(coffeeSources)
+        .pipe(coffee({
+                bare: true
+            })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+
 });
