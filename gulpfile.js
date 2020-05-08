bb2e67738e7d366910d7becef26fdb75dceaac0c
unknown <osullit@hotmail.com>
2020-05-08 15:03:23 -0600

    Added compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
@@ -9,6 +10,8 @@ var jsSources = ['components/scripts/rclick.js',
 'components/scripts/pixgrid.js',
 'components/scripts/tagline.js',
 'components/scripts/template.js'];
+
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', async function () {
     gulp.src(coffeeSources)
@@ -25,4 +28,17 @@ gulp.task('js', async function () {
         .pipe(concat('scripts.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', async function () {
+    gulp.src(sassSources)
+        .pipe(compass({
+                sass: 'components/sass',
+                image: 'builds/development/images',
+                style: 'expanded'
+            })
+            .on('error', gutil.log)
+
+        )
+        .pipe(gulp.dest('builds/development/css'))
 });
